7762821b74eb8bdc67b903e0c71ef45339c6f62f
ize-G <gominahblessing@outlook.com>
2022-06-30 16:43:25 -0700

    Dispays text with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Retur: Always 0 (Success)
- * */
-int main (void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 
 return(0);
 }
