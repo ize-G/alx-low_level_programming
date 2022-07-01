9d01d5578ad7281939f0530d6413ff13aed8ea3c
ize-G <gominahblessing@outlook.com>
2022-07-01 02:25:05 -0700

    Displays quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
-#include <stdio.h>
 
 /**
  * main - Prints a standard error
  *
  * Return: 1 (Success)
  */
-
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2,"and that peice of art is useful\"- Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }	
