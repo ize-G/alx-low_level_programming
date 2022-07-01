aa8244741fd22a71a853e63fd473aaad613971d8
ize-G <gominahblessing@outlook.com>
2022-07-01 01:47:39 -0700

    Task 6

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a standard error
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	printf(stderr, "and that piece of art is useful\" - \Dora Korpar, 2015-10-19\n");
+	return (1);
+}	
