f780fc198a9643d7c0acc2efd7f3034ba8d80f8c
ize-G <gominahblessing@outlook.com>
2022-07-02 07:30:38 -0700

    prints base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i;
+	char ch;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
