e20865454477276e4dc7b4d79c889bc0980104fd
ize-G <gominahblessing@outlook.com>
2022-07-02 08:40:09 -0700

    Prints combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int o;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (o = 50; o < 58; o++)
+			{
+				if (o > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(o);
+				if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
