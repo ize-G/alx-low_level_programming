0e6eaa19a7a158608bfc7c24dd31a1a97450df48
ize-G <gominahblessing@outlook.com>
2022-07-06 12:41:53 +0000

    Prints the alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet- prints the alphabet in lower case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
