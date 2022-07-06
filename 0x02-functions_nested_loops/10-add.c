7809372ba84641eac358a2c9658f663c84ffd07a
ize-G <gominahblessing@outlook.com>
2022-07-06 15:11:02 +0000

    Add two parameters of type int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add- adds two integers and returns the result
+ * @n1: first parameter of type int to be added
+ * @n2: second parameter of type int to be added
+ *
+ * Return: the added integers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
