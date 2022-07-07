941a0b3abc9796a37b19d0c7bb694f2fd4189d4f
ize-G <gominahblessing@outlook.com>
2022-07-07 17:46:01 +0000

    Next main.c test file

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* Prints out whether the stored number is +ve or-ve */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+
+		if (n > 0)
+		{
+			printf("%d is positive\n", n);
+		}
+		else if (n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+		else
+		{
+			printf("%i is negative\n", n);
+		}
+		return (0);
+}
