91116917272a840b98e8eb6729d8ca8dbaa49907
ize-G <gominahblessing@outlook.com>
2022-07-08 10:07:30 +0000

    Take x+1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,6 +24,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+	else if (b >= c && c <= a)
+	{
+		largest = b;
+	}
+	else if (c >= a && a >= b)
+	{
+		largest = c;
+	}
 	else
 	{
 		largest = c;
