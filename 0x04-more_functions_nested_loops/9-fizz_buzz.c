940f72f0db29d5c59ecdf7010b6851396a0c8f36
ize-G <gominahblessing@outlook.com>
2022-07-10 15:26:43 +0000

    Take 4 Fizzbuzz hotfix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,7 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 != 0) && (i % 5 != 0))
-{
-printf("%d ", i);
-}
-else if ((i % 3 == 0) && (i % 5 == 0))
+if (i % 15 == 0)
 {
 printf("FizzBuzz ");
 }
@@ -30,6 +26,8 @@ else if (i % 5 == 0)
 {
 printf("Buzz ");
 }
+else
+printf("%i ", i);
 }
 printf("\n");
 return (0);
