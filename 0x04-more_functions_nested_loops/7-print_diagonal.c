97c9734b28246a60fd1d7840b52048ea08e569ee
ize-G <gominahblessing@outlook.com>
2022-07-10 09:06:11 +0000

    Prints a diagonal line of length "n"; previous code was printing an additional line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,25 +7,24 @@
  */
 void print_diagonal(int n)
 {
-	if (n > 0)
-	{
-		int i;
+if (n > 0)
+{
+int i;
 
-		for (i = 0; i < n; i++)
-		{
-			int b;
+for (i = 0; i < n; i++)
+{
+int b;
 
-			for (b = 0; b < i; b++)
-			{
-				_putchar(' ');
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
-	_putchar('\n');
+for (b = 0; b < i; b++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
