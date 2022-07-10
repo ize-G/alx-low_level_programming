e1d98d970917bbbe97d3e6e1cecc1c0dbfcdf187
ize-G <gominahblessing@outlook.com>
2022-07-10 16:40:53 +0000

    Prints integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,20 @@
  */
 void print_number(int n)
 {
-unsigned int i = n;
+unsigned int i;
 
 if (n < 0)
 {
 _putchar(45);
-i = -i;
+i = -n;
 }
-if ((i / 10) > 0)
+else
+{
+i = n;
+}
+if (i / 10)
 {
 print_number(i / 10);
 }
-_putchar((i % 10) + 48);
+_putchar((i % 10) + '0');
 }
