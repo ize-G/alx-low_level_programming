1714c5fc2b9e9dba2c03340c1b9700ca78700d2d
ize-G <gominahblessing@outlook.com>
2022-07-10 10:06:21 +0000

    Prints a newline if size is less than 0

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,19 @@ void print_square(int size)
 int width;
 int length;
 
+if (size > 0)
+{
 for (length = 0; length < size; length++)
 {
 for (width = 0; width < size; width++)
 {
 _putchar(35);
 }
+_putchar('\n');
+}
+}
+else
+{
 _putchar('\n');
 }
 }
