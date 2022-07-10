9f1fbb088a687315d6ea66bc0e1f945090270bd3
ize-G <gominahblessing@outlook.com>
2022-07-10 15:03:48 +0000

    Fizzbuzz hotfix

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main- Prints the number 1 to 10 followed by a new line
+ *	for multiples of 3 print Fizz
+ *	for multiples of 5 print Buzz
+ *	for multiples of both 3 and 5 print FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3 != 0) && (i % 5 != 0))
+		{
+			printf("%d ", i);
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf("Buzz ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
