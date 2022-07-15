1ee92b320e71e18a6ff49bf35bc43fee5e93da95
ize-G <gominahblessing@outlook.com>
2022-07-15 11:19:58 +0000

    Prints a sentence in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_rev -  prints a string in reverse
+ *
+ * @s: string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
