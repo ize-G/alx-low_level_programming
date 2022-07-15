bc3beceebf2dccb69651689959d0ee6af3d386ba
ize-G <gominahblessing@outlook.com>
2022-07-15 08:26:17 +0000

    Task 0 - using a point in deferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98- Takes a pointer to an int as parameter
+ * and updates the value it points to 98.
+ *
+ * @n: The variable to be reset using the pointer
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
