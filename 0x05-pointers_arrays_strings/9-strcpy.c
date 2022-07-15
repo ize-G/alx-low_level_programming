4b8e90e2f35ba2d3fcd1c7750b0061d694ad0a21
ize-G <gominahblessing@outlook.com>
2022-07-15 18:34:00 +0000

    Task 9- copies string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string from one dereferenced location to another
+ * @dest: destination to be copied to
+ * @src: destination to be copied from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+
+	while (src[n] != '\0')
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	dest[n] = '\n';
+
+	return (dest);
+}
