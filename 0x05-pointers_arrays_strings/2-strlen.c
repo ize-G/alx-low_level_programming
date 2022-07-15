561b5c5503052a4949eacacf3f9838f9aa77f299
ize-G <gominahblessing@outlook.com>
2022-07-15 09:11:36 +0000

    Task 2- computes the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - computes the length of a string
+ *
+ * @s: string whose lenght would be computed
+ *
+ * Return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0; 
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
