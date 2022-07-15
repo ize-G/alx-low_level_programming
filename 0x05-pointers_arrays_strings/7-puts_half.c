45e376c57866c970372e1980636a34c9dd4b3f4d
ize-G <gominahblessing@outlook.com>
2022-07-15 16:54:11 +0000

    Prints half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string,
+ *	followed by a newline.
+ * @str: string to be computed
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length_of_the_string = 0;
+	int n; /* n is the midpoint */
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length_of_the_string++;
+	}
+
+	if (length_of_the_string % 2 == 0)
+	{
+		for (n = (length_of_the_string / 2); (str[n] != '\0'); n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+
+	else
+	{
+		for (n = ((length_of_the_string - 1) / 2); (str[n] != '\0'); n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
