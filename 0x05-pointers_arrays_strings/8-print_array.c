f1cad18618f1c127a2d430480561620ac9063adb
ize-G <gominahblessing@outlook.com>
2022-07-15 17:46:52 +0000

    Task 8- prints array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - prints out all the elements in an array
+* @a: array to be printed
+* @n: number pf array elements to print
+*
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
