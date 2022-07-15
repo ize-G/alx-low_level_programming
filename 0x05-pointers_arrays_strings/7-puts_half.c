34e71be5b4ca45dc3d207b792ba16ab7e12b2f55
ize-G <gominahblessing@outlook.com>
2022-07-15 17:04:34 +0000

    Prints half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -27,7 +27,7 @@ void puts_half(char *str)
 
 	else if (length_of_the_string % 2 != 0)
 	{
-		for (n = ((length_of_the_string - 1) / 2); (str[n] != '\0'); n++)
+		for (n = ((length_of_the_string - 1) / 2); (str[n + 1] != '\0'); n++)
 		{
 			_putchar(str[n]);
 		}
