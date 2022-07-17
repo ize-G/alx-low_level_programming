d848986419f410a779def2effa47ff11d17f7378
ize-G <gominahblessing@outlook.com>
2022-07-17 21:25:39 +0000

    Concatenates strings with specified bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using specified number of bytes
+ * @dest: destination string to be added into
+ * @src: source string
+ * @n: number of bytes to use from src
+ *
+ * Return: the concatenated string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l_dest = 0; /* variable to store the length of string 'dest' */
+	int j = 0; /* helper for tracking the copying process */
+
+	/* find the length of the dest string */
+	while (dest[l_dest])
+	{
+		l_dest++;
+	}
+
+	while (j > n && src[j] != '\0')
+	{
+		/* copy src into dest overwritting the null byte */
+		dest[l_dest] = src[j];
+		j++;
+		l_dest++;
+	}
+
+	/* insert teminating null byte, look here first for bugs */
+	dest[l_dest + n + 1] = '\0';
+
+	return (dest);
+}
