fe84a4b07cdb78de62afbcab41bf7a3f0b856c98
ize-G <gominahblessing@outlook.com>
2022-07-18 16:17:28 +0000

    Captitalizes the first letter of a word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,21 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
-char s[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '{'};
+int i;
+char s[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 int a = 0;
 int len = 13;
 
-while (str[i] != '\0')
+while (str[i])
 {
-while (a < len)
-{
-if ((i == 0 || str[i - 1] == s[a]) && str[i] >= 97 && str[i] <= 122)
-
-{
-str[i] = str[i] - 32;
+	for (a = 0; a < len; a++)
+	{
+		if ((i == 0 || str[i - 1] == s[a]) && (str[i] >= 97 && str[i] <= 122))
+		{
+			str[i] = str[i] - 32;
+			++i;
+		}
+	}
 }
-i++;
-}
-a++;
-
-}
-
 return (str);
 }
---
+++ 0x06-pointers_arrays_strings/sizeof.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- checks the size of an char
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	a = sizeof(char);
+	printf("%d,\n", a);
+
+	return (0);
+}
+
+
