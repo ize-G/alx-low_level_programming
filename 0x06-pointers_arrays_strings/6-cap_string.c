2a46bf57909b463a325e28c1ea9d5b32358f9cf9
ize-G <gominahblessing@outlook.com>
2022-07-19 11:59:12 +0000

    Capitalizes each word in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,27 @@
  */
 char *cap_string(char *str)
 {
-int i, a;
-char s[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int i = 0;
+int a = 0;
+char s[] = {'\t', ' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-while (str[i])
-{
-	if (i == 0 && (str[i] >= 97 && str[i] <= 122))
-	{
-		str[i] = str[i] - 32;
-		++i;
-	}
-
-	for (a = 0; a < 13; ++a)
+	while (str[i])
 	{
 
-		if (s[a] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+		if (i == 0 && (str[i] >= 97 && str[i] <= 122))
 		{
-			str[i + 1] = str[i + 1] - 32;
+			str[i] = str[i] - 32;
 		}
+
+		for (a = 0; (s[a]); a++)
+		{
+			if (s[a] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+
+i++;
 	}
-}
-return (str);
+	return (str);
 }
