189993abbc81a498c940ef5a1215b935cb780dfa
ize-G <gominahblessing@outlook.com>
2022-07-29 15:35:31 +0000

    Prints the sum of the left and right diagonal of a square matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a
+ *	matrix of integers.
+ *@a: the matrix of integers to be computed
+ *@size: the size of a.
+ * Return: nothing.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int left_diag, right_diag;
+
+	for (i = 0; i < size; i++)
+	{
+		left_diag += a[(size * i) + i];
+		right_diag += a[(size * (i + 1)) - (i + 1)];
+	}
+
+	printf("%d, %d\n", left_diag, right_diag);
+}
